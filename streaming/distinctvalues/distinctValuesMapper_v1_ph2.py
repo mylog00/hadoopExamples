7108a1a0c618e020fd93ff0e1bbf8681095bbdc6
mylog00 <mylog00@ya.ru>
2016-06-13 21:23:13 +0500

    Distinct-values mapper (v1, phase 2);

---
+++ streaming/distinctvalues/distinctValuesMapper_v1_ph2.py
@@ -0,0 +1,23 @@
+# Sample Input:
+# 1,a
+# 2,a
+# 3,a
+# 1,b
+# 3,b
+# 2,d
+# 2,e
+
+# Sample Output:
+# a	1
+# a	1
+# a	1
+# b	1
+# b	1
+# d	1
+# e	1
+
+import sys
+
+for line in sys.stdin:
+    v = line.strip().split(',')[1]
+    print(v, '1', sep='\t')
