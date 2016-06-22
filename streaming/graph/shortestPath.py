82e5282ecfab3b04c88e9fc7be53f56e8cc02ed1
mylog00 <mylog00@ya.ru>
2016-06-22 20:52:18 +0500

    Extract priority queue to class

--- streaming/graph/shortestPath.py
+++ streaming/graph/shortestPath.py
@@ -18,6 +18,40 @@ from heapq import heappush, heappop
 import itertools
 
 
+class PriorityQueue:
+    def __init__(self):
+        self.pq = []  # list of entries arranged in a heap
+        self.entry_finder = {}  # mapping of tasks to entries
+        self.REMOVED = '<removed-task>'  # placeholder for a removed task
+        self.counter = itertools.count()  # unique sequence count
+
+    def push(self, task, priority=0.0):
+        'Add a new task or update the priority of an existing task'
+        if task in self.entry_finder:
+            self.remove_task(task)
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
+        heappush(self.pq, entry)
+
+    def remove_task(self, task):
+        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
+        entry = self.entry_finder.pop(task)
+        entry[-1] = self.REMOVED
+
+    def pop(self):
+        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.pq:
+            priority, count, task = heappop(self.pq)
+            if task is not self.REMOVED:
+                del self.entry_finder[task]
+                return task
+        raise KeyError('pop from an empty priority queue')
+
+    def empty(self):
+        return len(self.entry_finder) <= 0
+
+
 class DirectedEdge:
     def __init__(self, w_from, w_to, weight=0.0):
         self.__v_from = w_from
@@ -32,42 +66,6 @@ class DirectedEdge:
 
     def get_weight(self):
         return self.__weight
-
-
-pq = []  # list of entries arranged in a heap
-entry_finder = {}  # mapping of tasks to entries
-REMOVED = '<removed-task>'  # placeholder for a removed task
-counter = itertools.count()  # unique sequence count
-
-
-def push(task, priority=0.0):
-    'Add a new task or update the priority of an existing task'
-    if task in entry_finder:
-        remove_task(task)
-    count = next(counter)
-    entry = [priority, count, task]
-    entry_finder[task] = entry
-    heappush(pq, entry)
-
-
-def remove_task(task):
-    'Mark an existing task as REMOVED.  Raise KeyError if not found.'
-    entry = entry_finder.pop(task)
-    entry[-1] = REMOVED
-
-
-def pop():
-    'Remove and return the lowest priority task. Raise KeyError if empty.'
-    while pq:
-        priority, count, task = heappop(pq)
-        if task is not REMOVED:
-            del entry_finder[task]
-            return task
-    raise KeyError('pop from an empty priority queue')
-
-
-def empty():
-    return len(entry_finder) <= 0
 
 
 array = input().split(' ')
@@ -95,17 +93,18 @@ for i in range(vertex):
     distTo.append(float('inf'))
 
 distTo[v_from] = 0
-push(v_from, 0)
+pq = PriorityQueue()
+pq.push(v_from, 0)
 
-while not empty():
-    next_v = pop()
+while not pq.empty():
+    next_v = pq.pop()
     for next_e in graph[next_v]:
         prev_dist = distTo[next_e.get_to()]
         next_dist = distTo[next_e.get_from()] + next_e.get_weight()
         if prev_dist > next_dist:
             distTo[next_e.get_to()] = next_dist
             edgeTo[next_e.get_to()] = next_e
-            push(next_e.get_to(), next_dist)
+            pq.push(next_e.get_to(), next_dist)
 
 if distTo[v_to] == float('inf'):
     print('-1')
